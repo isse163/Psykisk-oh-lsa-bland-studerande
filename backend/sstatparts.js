e2532d018a6e726f944bc2cf803c53286d7900b6
isse163 <issabd123@outlook.com>
2025-05-08 14:07:44 +0200

    commit

--- backend/sstatparts.js
+++ backend/sstatparts.js
@@ -1,5 +1,8 @@
 import sqlite3 from 'sqlite3';
-
+sqlite3.verbose();
+import { open } from 'sqlite';
+import { join } from 'path';
+import { fileURLToPath } from 'url';
 export function getSleepVsDepression(db) {
   return new Promise((resolve, reject) => {
     db.all(`SELECT "Sleep Duration", Depression FROM student_health`, [], (err, rows) => {
